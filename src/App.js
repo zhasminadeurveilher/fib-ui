a8e08c424432edb27c1738059bf1b97c902eba19
Zhasmina Stoyanova <zhasmina.stoyanova@gmail.com>
2023-07-11 17:13:18 +0200

    #4 Add new page on Create New

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CurrentAccounts from './pages/CurrentAccounts';
+import NewCurrentAccount from './pages/NewCurrentAccount';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<CurrentAccounts />} />
+          <Route exact path='/newcurrentaccount' element={<NewCurrentAccount />} />
         </Routes>
       </Router>
     </div>
